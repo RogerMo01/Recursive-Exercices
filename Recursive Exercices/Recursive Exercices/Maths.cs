4f584a745faa668f3806a20e6053dbc83df911a4
Roger <morenoroger001@gmail.com>
2020-10-30 21:25:55 -0400

    refactor: Use ternary operator

--- Recursive Exercices/Recursive Exercices/Maths.cs
+++ Recursive Exercices/Recursive Exercices/Maths.cs
@@ -10,10 +10,7 @@ namespace Recursive_Exercices
     {
         public static int Factorial(int number)
         {
-            if (number == 1)
-                return 1;
-
-            return number * Factorial(number - 1);
+            return (number == 1) ? 1 : number * Factorial(number - 1);
         }
     }
 }
