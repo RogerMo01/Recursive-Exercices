b7998f52df8568b19006fc9170baf025e9f6792f
Roger <morenoroger001@gmail.com>
2020-10-30 20:00:17 -0400

    feat: Add Factorial method to Maths.cs

--- Recursive Exercices/Recursive Exercices/Maths.cs
+++ Recursive Exercices/Recursive Exercices/Maths.cs
@@ -8,5 +8,12 @@ namespace Recursive_Exercices
 {
     class Maths
     {
+        public static int Factorial(int number)
+        {
+            if (number == 1)
+                return 1;
+
+            return number * Factorial(number - 1);
+        }
     }
 }
