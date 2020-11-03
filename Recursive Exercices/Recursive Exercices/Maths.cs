881d0bf484db60cced3798c70c9fc618a56bb0f1
Roger <morenoroger001@gmail.com>
2020-11-02 21:51:10 -0500

    feat: Add Fibonacci method in Maths.cs

--- Recursive Exercices/Recursive Exercices/Maths.cs
+++ Recursive Exercices/Recursive Exercices/Maths.cs
@@ -8,5 +8,12 @@ namespace Recursive_Exercices
 {
     class Maths
     {
+        public static int Fibonacci(int positionInSequence)
+        {
+            if (positionInSequence == 1) { return 0; }
+            if (positionInSequence == 2) { return 1; }
+
+            return Fibonacci(positionInSequence - 1) + Fibonacci(positionInSequence - 2);
+        }
     }
 }
