f88015ecb4061ffab62ec935afadd041c66e0a66
Roger <morenoroger001@gmail.com>
2020-11-04 00:01:13 -0500

    feat: Add RankTo method to Maths class

--- Recursive Exercices/Recursive Exercices/Maths.cs
+++ Recursive Exercices/Recursive Exercices/Maths.cs
@@ -8,5 +8,18 @@ namespace Recursive_Exercices
 {
     class Maths
     {
+        public static int[] RankTo(int top)
+        {
+            return FillArray(top, new int[top + 1]);
+        }
+        private static int[] FillArray(int top, int[] list)
+        {
+            list[top] = top;
+
+            if (top == 0)
+            { return list; }
+
+            return FillArray(top - 1, list);
+        }
     }
 }
