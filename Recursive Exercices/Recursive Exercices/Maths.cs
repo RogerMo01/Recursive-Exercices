971b75e9364446f35c32b8e962794c1ccedf51c3
Roger <morenoroger001@gmail.com>
2020-11-06 09:52:57 -0500

    feat: Add RankBetween method to Maths.cs

--- Recursive Exercices/Recursive Exercices/Maths.cs
+++ Recursive Exercices/Recursive Exercices/Maths.cs
@@ -8,5 +8,20 @@ namespace Recursive_Exercices
 {
     class Maths
     {
+        public static List<int> RankBetween(int bottom, int top)
+        {
+            List<int> list = new List<int>();
+            return FillArray(list, bottom, top);
+        }
+        
+        private static List<int> FillArray(List<int> list, int bottom, int top)
+        {
+            list.Add(bottom);
+
+            if (bottom == top)
+            { return list; }
+
+            return FillArray(list, bottom + 1, top);
+        }
     }
 }
