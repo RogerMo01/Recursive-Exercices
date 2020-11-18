5c1e041cb21d43085597dc0c3710617ecaefb19b
Roger <morenoroger001@gmail.com>
2020-11-17 23:53:04 -0500

    feat: add ToUppercase class

---
+++ Recursive Exercices/Recursive Exercices/ToUppercase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Exercices
+{
+    class ToUppercase
+    {
+        public static string ToUpperC(string word)
+        {
+            return Convert(word, "");
+        }
+
+        private static string Convert(string word, string returnWord)
+        {
+            if (returnWord.Count() == word.Count())
+            {
+                return returnWord;
+            }
+
+            return Convert(word, returnWord.Insert(returnWord.Count(), word.ElementAt(returnWord.Count()).ToString().ToUpper()));
+        }
+    }
+}
